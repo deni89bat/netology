9feae66072dd422d384ae3b2983cec5d0175d549
deni89bat <den.pm14@gmail.com>
2023-02-20 17:14:28 +0300

    3 commi

--- netology/src/Main.java
+++ netology/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("commi2!");
         System.out.println("Hello world!");
+        System.out.println("commi3!");
+        System.out.println("commi4!");
     }
 }
