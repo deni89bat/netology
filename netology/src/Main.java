f1dc99b58257be8ffcf4b43e167421f2e02c4e53
deni89bat <den.pm14@gmail.com>
2023-02-20 18:09:39 +0300

    Revert "3 commi"
    This reverts commit 9feae66072dd422d384ae3b2983cec5d0175d549.

--- netology/src/Main.java
+++ netology/src/Main.java
@@ -2,7 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("commi2!");
         System.out.println("Hello world!");
-        System.out.println("commi3!");
-        System.out.println("commi4!");
     }
 }
