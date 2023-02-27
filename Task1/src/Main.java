57131784ae9da0044c4cf710f5ff83bb63426a17
deni89bat <den.pm14@gmail.com>
2023-02-27 19:24:24 +0300

    init commit

---
+++ Task1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long customerAccount = 2000000000;
+         long transferAmount = 500000000;
+        long total = customerAccount + transferAmount;
+        System.out.println("сумма " + total);
+    }
+}
